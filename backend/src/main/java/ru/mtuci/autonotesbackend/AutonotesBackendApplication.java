e641417d6272dad6142c3dae0007904f20dbe70e
Said <alievsaid0909@yandex.ru>
2025-12-03 03:42:03 +0300

    feat(core): включена поддержка планировщика задач (@EnableScheduling)

--- backend/src/main/java/ru/mtuci/autonotesbackend/AutonotesBackendApplication.java
+++ backend/src/main/java/ru/mtuci/autonotesbackend/AutonotesBackendApplication.java
@@ -3,9 +3,11 @@ package ru.mtuci.autonotesbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching
+@EnableScheduling
 public class AutonotesBackendApplication {
 
     public static void main(String[] args) {
