5f505d2b4379bd722fc78276c89889db1e5fbb37
Said <alievsaid0909@yandex.ru>
2025-12-03 04:26:27 +0300

    test(integration): добавлен RabbitMQContainer в базовый класс тестов

--- backend/src/test/java/ru/mtuci/autonotesbackend/BaseIntegrationTest.java
+++ backend/src/test/java/ru/mtuci/autonotesbackend/BaseIntegrationTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.MinIOContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
@@ -31,29 +32,34 @@ public abstract class BaseIntegrationTest {
     static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
         static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
-
         static MinIOContainer minio = new MinIOContainer("minio/minio:RELEASE.2025-09-07T16-13-09Z-cpuv1");
+        static RabbitMQContainer rabbit = new RabbitMQContainer(DockerImageName.parse("rabbitmq:3.12-management-alpine"));
 
         @Override
         public void initialize(ConfigurableApplicationContext applicationContext) {
             postgres.start();
             minio.start();
+            rabbit.start();
 
             TestPropertyValues.of(
-                            "spring.datasource.url=" + postgres.getJdbcUrl(),
-                            "spring.datasource.username=" + postgres.getUsername(),
-                            "spring.datasource.password=" + postgres.getPassword(),
-                            "spring.flyway.url=" + postgres.getJdbcUrl(),
-                            "spring.flyway.user=" + postgres.getUsername(),
-                            "spring.flyway.password=" + postgres.getPassword(),
-                            "JWT_SECRET=dGVzdC1zZWNyZXQtZm9yLWp3dC10ZXN0aW5nLWxvbmctZW5vdWdo",
-                            "JWT_EXPIRATION_MS=86400000",
-                            "aws.s3.endpoint=" + minio.getS3URL(),
-                            "aws.s3.access-key=" + minio.getUserName(),
-                            "aws.s3.secret-key=" + minio.getPassword(),
-                            "aws.s3.bucket=test-bucket",
-                            "aws.s3.region=us-east-1")
-                    .applyTo(applicationContext.getEnvironment());
+                    "spring.datasource.url=" + postgres.getJdbcUrl(),
+                    "spring.datasource.username=" + postgres.getUsername(),
+                    "spring.datasource.password=" + postgres.getPassword(),
+                    "spring.flyway.url=" + postgres.getJdbcUrl(),
+                    "spring.flyway.user=" + postgres.getUsername(),
+                    "spring.flyway.password=" + postgres.getPassword(),
+                    "JWT_SECRET=dGVzdC1zZWNyZXQtZm9yLWp3dC10ZXN0aW5nLWxvbmctZW5vdWdo",
+                    "JWT_EXPIRATION_MS=86400000",
+                    "aws.s3.endpoint=" + minio.getS3URL(),
+                    "aws.s3.access-key=" + minio.getUserName(),
+                    "aws.s3.secret-key=" + minio.getPassword(),
+                    "aws.s3.bucket=test-bucket",
+                    "aws.s3.region=us-east-1",
+                    "spring.rabbitmq.host=" + rabbit.getHost(),
+                    "spring.rabbitmq.port=" + rabbit.getAmqpPort(),
+                    "spring.rabbitmq.username=" + rabbit.getAdminUsername(),
+                    "spring.rabbitmq.password=" + rabbit.getAdminPassword())
+                .applyTo(applicationContext.getEnvironment());
         }
     }
 }
